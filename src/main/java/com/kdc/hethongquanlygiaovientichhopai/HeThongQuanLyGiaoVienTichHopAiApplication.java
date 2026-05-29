ac17856359f3ed0a988a1258523475b205f57565
MinhHoangCoder <hoangmaihaminh@gmail.com>
2026-05-29 22:07:52 +0700

    update project base & project database

---
+++ src/main/java/com/kdc/hethongquanlygiaovientichhopai/HeThongQuanLyGiaoVienTichHopAiApplication.java
@@ -0,0 +1,13 @@
+package com.kdc.hethongquanlygiaovientichhopai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongQuanLyGiaoVienTichHopAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeThongQuanLyGiaoVienTichHopAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdc/hethongquanlygiaovientichhopai/HeThongQuanLyGiaoVienTichHopAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdc.hethongquanlygiaovientichhopai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeThongQuanLyGiaoVienTichHopAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
